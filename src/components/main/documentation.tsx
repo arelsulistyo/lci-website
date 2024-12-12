849a325a646534fb3bbdd8fe3d64838c9dc663a3
Rhafael Chandra <109317539+rhafaelc@users.noreply.github.com>
2024-12-12 19:27:43 +0700

    feat: final vid

--- src/components/main/documentation.tsx
+++ src/components/main/documentation.tsx
@@ -23,7 +23,7 @@ function VideoSection() {
         <div className="relative flex-1 bg-neutral-200 dark:bg-slate-800/[0.8] rounded">
           <div className="w-full h-0" style={{ paddingBottom: "56.25%" }}></div>
           <iframe
-            src="https://drive.google.com/file/d/1MFrDT_bH7p54QTiaVengma0AAeMrznqa/preview"
+            src="https://drive.google.com/file/d/1iKeXDtPeiOoPGICx8TcCqwaKUp2YpwAW/preview"
             allowFullScreen
             allow="autoplay"
             className="absolute top-0 left-0 w-full h-full p-4"></iframe>
@@ -31,7 +31,7 @@ function VideoSection() {
         <h3 className="flex-1 scroll-m-20 text-2xl font-semibold tracking-tight">
           This is some text content that will occupy the rest of the space.{" "}
           <LinkPreview
-            url="https://drive.google.com/file/d/1MFrDT_bH7p54QTiaVengma0AAeMrznqa/edit"
+            url="https://drive.google.com/file/d/1iKeXDtPeiOoPGICx8TcCqwaKUp2YpwAW/view"
             className="font-bold bg-clip-text text-transparent bg-gradient-to-br from-purple-500 to-pink-500">
             <Paperclip
               className="w-6 h-6 inline-block dark:invert"
