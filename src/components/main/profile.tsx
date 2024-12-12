515b6e9dc0e704052a1d6f5805ce88bd15d0e790
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-12-12 21:55:27 +0700

    fix: update profile images and remove emoji from names

--- src/components/main/profile.tsx
+++ src/components/main/profile.tsx
@@ -6,11 +6,11 @@ export function Profile() {
       crew: "Research Crew",
       name: "Nur Aziz Tri Indrawan",
       id: "21/474039/PA/20458",
-      src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
+      src: "https://drive.usercontent.google.com/download?id=19_EvR9mbSE__1v90IDjGNXC9mY2dQ-zc",
     },
     {
       crew: "Film Crew",
-      name: "Dewangga Haryono Putra 😋",
+      name: "Dewangga Haryono Putra",
       id: "22/492661/PA/21132",
       src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
     },
@@ -24,13 +24,13 @@ export function Profile() {
       crew: "Research Crew",
       name: "Tengku Syaid Farhan",
       id: "22/493998/PA/21234",
-      src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
+      src: "https://drive.usercontent.google.com/download?id=1y1tOLnOQsSi2-bn3SQMYop4N_EiUwiS3",
     },
     {
       crew: "Film Crew",
       name: "Dinda Hana Zhafira",
       id: "22/494038/PA/21240",
-      src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
+      src: "https://drive.usercontent.google.com/download?id=1Kk5CYcLWbgZ64S1Wp98r-398HX9bx8I9",
     },
     {
       crew: "Research Crew",
@@ -42,7 +42,7 @@ export function Profile() {
       crew: "Website Crew",
       name: "Rakyan Pangrukti Wibana",
       id: "22/497076/PA/21394",
-      src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
+      src: "https://drive.usercontent.google.com/download?id=1AO3-ABO3tkorhZQn1Y_mGNyTO_CGDcah",
     },
     {
       crew: "Website Crew",
@@ -54,7 +54,7 @@ export function Profile() {
       crew: "Film Crew",
       name: "Zulfia Dwi Al Syalwa",
       id: "22/497584/PA/21428",
-      src: "https://drive.usercontent.google.com/download?id=1YqoeD9Kh8X99Ye7S5gzP7gskZdcKYegG",
+      src: "https://drive.usercontent.google.com/download?id=1kdg67DyD0xwpmrXvd02cr__jVLar5q1f",
     },
     {
       crew: "Website Crew",
