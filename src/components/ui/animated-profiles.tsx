b70c81c98547bbe367968a86d9b2131b5472be5f
allsyalwaa <zulfiaalsyalwa@gmail.com>
2024-12-12 21:55:34 +0700

    refactor: enhance layout and styling of AnimatedProfiles component with improved button design

--- src/components/ui/animated-profiles.tsx
+++ src/components/ui/animated-profiles.tsx
@@ -44,8 +44,8 @@ export const AnimatedProfiles = ({
     return Math.floor(Math.random() * 21) - 10;
   };
   return (
-    <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20">
-      <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20">
+    <div className="max-w-sm md:max-w-4xl mx-auto antialiased font-sans px-4 md:px-8 lg:px-12 py-20 w-full">
+      <div className="relative grid grid-cols-1 md:grid-cols-2  gap-20 items-center">
         <div>
           <div className="relative h-80 w-full">
             <AnimatePresence>
@@ -76,7 +76,8 @@ export const AnimatedProfiles = ({
                     duration: 0.4,
                     ease: "easeInOut",
                   }}
-                  className="absolute inset-0 origin-bottom">
+                  className="absolute inset-0 origin-bottom"
+                >
                   <Image
                     src={testimonial.src}
                     alt={testimonial.name}
@@ -90,7 +91,7 @@ export const AnimatedProfiles = ({
             </AnimatePresence>
           </div>
         </div>
-        <div className="flex justify-between flex-col py-4">
+        <div className="flex  flex-col py-4 items-center">
           <motion.div
             key={active}
             initial={{
@@ -108,14 +109,15 @@ export const AnimatedProfiles = ({
             transition={{
               duration: 0.2,
               ease: "easeInOut",
-            }}>
-            <h3 className="text-2xl font-bold dark:text-white text-black">
+            }}
+          >
+            <h3 className="text-4xl font-bold dark:text-white text-black">
               {profiles[active].name}
             </h3>
-            <p className="text-sm text-gray-500 dark:text-neutral-500">
+            <p className="text-2xl text-gray-500 dark:text-neutral-500 font-medium mt-2">
               {profiles[active].id}
             </p>
-            <motion.p className="text-lg text-gray-500 mt-8 dark:text-neutral-300">
+            <motion.p className="text-xl font-semibold text-gray-500 mt-3 dark:text-neutral-300 ">
               {profiles[active].crew.split(" ").map((word, index) => (
                 <motion.span
                   key={index}
@@ -134,25 +136,28 @@ export const AnimatedProfiles = ({
                     ease: "easeInOut",
                     delay: 0.02 * index,
                   }}
-                  className="inline-block">
+                  className="inline-block"
+                >
                   {word}&nbsp;
                 </motion.span>
               ))}
             </motion.p>
           </motion.div>
-          <div className="flex gap-4 pt-12 md:pt-0">
-            <button
-              onClick={handlePrev}
-              className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
-              <IconArrowLeft className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:rotate-12 transition-transform duration-300" />
-            </button>
-            <button
-              onClick={handleNext}
-              className="h-7 w-7 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button">
-              <IconArrowRight className="h-5 w-5 text-black dark:text-neutral-400 group-hover/button:-rotate-12 transition-transform duration-300" />
-            </button>
-          </div>
         </div>
+      </div>
+      <div className="flex gap-4 pt-12  mx-auto mt-4 justify-center">
+        <button
+          onClick={handlePrev}
+          className="h-10 w-10 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button dark:hover:bg-white "
+        >
+          <IconArrowLeft className="h-5 w-5 text-black dark:text-neutral-400 " />
+        </button>
+        <button
+          onClick={handleNext}
+          className="h-10 w-10 rounded-full bg-gray-100 dark:bg-neutral-800 flex items-center justify-center group/button dark:hover:bg-white"
+        >
+          <IconArrowRight className="h-5 w-5 text-black dark:text-neutral-400 " />
+        </button>
       </div>
     </div>
   );
